a9da3be9219e7961adb3ed7f9c93d2e3d523d8d6
Kalyan Godavarthi <kalyan.godavarthi@infor.com>
2021-08-17 13:52:36 +0530

    formatted the code

--- app.py
+++ app.py
@@ -2,49 +2,51 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-names = [ { 'name' : "Kalyan G",
-             'Role' : "DevOps Engineer",
-             'employeeid' : "1"
-},
-{ 'name' : "Ryan J",
-             'Role' : "HR",
-             'employeeid' : "2"
-},
-{ 'name' : "John C",
-             'Role' : "DevOps Engineer",
-             'employeeid' : "3"
-},
-{ 'name' : "Joe A",
-             'Role' : "DevOps Engineer",
-             'employeeid' : "4"
-},
-{ 'name' : "Sidhu G",
-             'Role' : "DevOps Engineer",
-             'employeeid' : "5"
-}
-]
+names = [{'name': "Kalyan G",
+          'Role': "DevOps Engineer",
+          'employeeid': "1"
+          },
+         {'name': "Ryan J",
+          'Role': "HR",
+          'employeeid': "2"
+          },
+         {'name': "John C",
+          'Role': "DevOps Engineer",
+          'employeeid': "3"
+          },
+         {'name': "Joe A",
+          'Role': "DevOps Engineer",
+          'employeeid': "4"},
+         {'name': "Sidhu G",
+          'Role': "DevOps Engineer",
+          'employeeid': "5"}
+         ]
+
 
 @app.route('/')
 def index():
     return "Welcome to the reading API"
 
-@app.route("/names", methods = ['GET'])
-def get():
-    return jsonify({'names':names})
 
-@app.route("/names", methods = ['POST'])
+@app.route("/names", methods=['GET'])
+def get():
+    return jsonify({'names': names})
+
+
+@app.route("/names", methods=['POST'])
 def create():
-    name = { "name" : "Shiva ",
-            "Role" : "Manager",
-            'employeeid' : "6"
-    }
+    name = {"name": "Shiva ",
+            "Role": "Manager",
+            'employeeid': "6"}
     names.append(name)
     return jsonify({'Created': name})
+
 
 @app.route("/names/<int:employeeid>", methods=['PUT'])
 def names_update(employeeid):
     names[employeeid]['Role'] = 'Lead'
     return jsonify({'names': names[employeeid]})
+
 
 @app.route("/names/<int:employeeid>", methods=['DELETE'])
 def delete(employeeid):
@@ -53,4 +55,4 @@ def delete(employeeid):
 
 
 if __name__ == "__main__":
-    app.run('0.0.0.0',5000,debug=True)
+    app.run('0.0.0.0', 5000, debug=True)
